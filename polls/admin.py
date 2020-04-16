8ad4b73a52a3c033b6f903e972e36b99377acb45
Patryk Olechno <patrykoolechno@gmail.com>
2020-04-16 18:05:24 +0200

    Improved Admin site question form

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+    ('Question', {'fields' : ['question_text']}),
+    ('Date Info', {'fields' : ['pub_date'], 'classes' : ['collapse']}),
+    ]
+    inlines = [ChoiceInLine]
+
+    list_display = ( 'question_text','pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
